d06cf528527ee3dd6c8efa4a6cf5a7bdebc24fec
Igor Vecherinsky <IVecherinsky@ibagroup.eu>
2021-09-16 12:16:29 +0300

    refactor

--- WatchInfo.cs
+++ WatchInfo.cs
@@ -11,19 +11,14 @@ namespace WatchPortalFunction
     public static class WatchInfo
     {
         [FunctionName("WatchInfo")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            // Retrieve the model id from the query string
             string model = req.Query["model"];
 
-            // If the user specified a model id, find the details of the model of watch
             if (model != null)
             {
-                // Use dummy data for this example
                 dynamic watchinfo = new { Manufacturer = "abc", CaseType = "Solid", Bezel = "Titanium", Dial = "Roman", CaseFinish = "Silver", Jewels = 15 };
 
                 return new OkObjectResult($"Watch Details: {watchinfo.Manufacturer}, {watchinfo.CaseType}, {watchinfo.Bezel}, {watchinfo.Dial}, {watchinfo.CaseFinish}, {watchinfo.Jewels}");
